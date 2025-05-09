b4a5756b47ccac8c7030a1100f515b00a350d696
kaia15 <baotranongtran@gmail.com>
2025-05-09 16:55:02 -0400

    feat(TensorPy): add type to methods in Tensor class

--- tensor.py
+++ tensor.py
@@ -3,13 +3,18 @@ class Tensor:
     This class is designed & implemented for N-array in Python. 
     It can have basic operations including creation, manipulation, mathematical ops, and poly(s).
     """
-    def __init__(self,data):
+    def __init__(self,data: list) -> 'Tensor':
+        """
+        TO-DO
+        """
         self.data = data
         self.shape = self._get_shape(data)
 
-    def _get_shape(self,data):
+    def _get_shape(self,data: list) -> tuple:
+        """
+        TO-DO
+        """
         shape = []
-        print (data)
         while True:
             nxt = len(data)
             shape.append(nxt)
@@ -17,6 +22,51 @@ class Tensor:
             if isinstance(data, int): 
                 break
         return tuple(shape)
+
+    @classmethod
+    def ones(cls,shape: tuple) -> 'Tensor':
+        """
+        TO-DO 
+        """
+        if not shape or any(dim <= 0 for dim in shape):
+            raise ValueError(f"Cannot create Tensor with invalid shape {shape}")
+        
+        if len(shape) == 1:
+            return cls([1] * shape[0])
+        
+        return cls([cls.ones(shape[1:]).data for _ in range(shape[0])])
+    
+    @classmethod
+    def zeros(cls,shape: tuple) -> 'Tensor':
+        """
+        TO-DO
+        """
+        if not shape or any(dim <= 0 for dim in shape):
+            raise ValueError(f"")
+        
+        if len(shape) == 0:
+            return cls([0] * shape[0])
+        
+        return cls([cls.ones(shape[1:]).data for _ in range(shape[0])])
+
+    @classmethod
+    def array(cls,shape: tuple) -> 'Tensor':
+        pass
+
+    @classmethod
+    def empty(self):
+        pass
+
+    def transpose(self,new_shape):
+        pass
+
+    def reshape(self):
+        pass
+
+    def flatten(self):
+        pass
+
+
     
 class Test:
     def unittest():
