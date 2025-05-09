0584222c1d4f645aca475438ba258665d428e5f6
kaia15 <baotranongtran@gmail.com>
2025-05-09 16:11:09 -0400

    feat(TensorPy): add object creation and its shape

--- tensor.py
+++ tensor.py
@@ -1,5 +1,29 @@
 class Tensor:
-    def __init__(self):
-        pass
+    """
+    This class is designed & implemented for N-array in Python. 
+    It can have basic operations including creation, manipulation, mathematical ops, and poly(s).
+    """
+    def __init__(self,data):
+        self.data = data
+        self.shape = self._get_shape(data)
+
+    def _get_shape(self,data):
+        shape = []
+        print (data)
+        while True:
+            nxt = len(data)
+            shape.append(nxt)
+            data = data[0]  # need to check whether it creates a new copy of input data or it points to the original input data 
+            if isinstance(data, int): 
+                break
+        return tuple(shape)
+    
+class Test:
+    def unittest():
+        data = [[[1,2,3]]]
+        t = Tensor(data)
+        print (t.shape)
+
+Test.unittest()
 
     
